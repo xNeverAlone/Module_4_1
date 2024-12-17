006b5c61f8d0351ef57b9ee82f7da08d811d18d9
xNeverAlone <barretsv@gmail.com>
2024-12-17 19:34:56 +0200

    Initial commit

---
+++ Module_4_1.py
@@ -0,0 +1,7 @@
+from Fake_math import divide as fk_div
+from True_math import divide as tr_div
+print('result1 = ', fk_div(12, 4))
+print('result2 = ', fk_div(12, 0))
+print('result3 = ', tr_div(13, 5))
+print('result4 = ', tr_div(13, 0))
+
---
+++ True_math.py
@@ -0,0 +1,8 @@
+import math
+from math import inf
+def divide(first, second):
+    if second == 0:
+        result = inf
+    else:
+        result = first / second
+    return result
